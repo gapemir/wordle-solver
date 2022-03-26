6c9fb84a2e5de3046ed009c5249b8c75dde1e259
Gašper <48834781+gapi2112@users.noreply.github.com>
2022-03-27 00:08:23 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -12,6 +12,10 @@ using namespace std;
 
 #define A 2315
 
+// spremenil v nize
+// spremeni samo 1. crko v '|'
+// narjeno za crne crke
+
 void cls() {
     // system("cls");
     COORD topLeft = {0, 0};
@@ -33,18 +37,20 @@ void barva(int i) { // 96 rumena 32 zelena 15 crna
     SetConsoleTextAttribute(hConsole, i);
 }
 
-void load_file(char tab1[A][5]) {
+void load_file(char tab1[A][6]) {
     ifstream inFile("besede_raw.txt");
     for (int i = 0; i < A; i++) {
-        for (int j = 0; j < 5; j++) {
-            inFile >> tab1[i][j];
+        for (int j = 0; j < 6; j++) {
+            if (j < 5)
+                inFile >> tab1[i][j]; // loda v prve 5 slotov
+            else
+                tab1[i][5] = '\0'; // v 6.ga da \0
         }
     }
-    // cout<<endl;
 }
 
-void switc(char arr[5], char arr2[5]) {
-    char t[5];
+void switc(char arr[6], char arr2[6]) {
+    char t[6];
     for (int i = 0; i < 5; i++) {
         t[i] = arr[i];
         arr[i] = arr2[i];
@@ -52,120 +58,115 @@ void switc(char arr[5], char arr2[5]) {
     }
 }
 
-void sort(char tab[A][5]) { // ce je place 1 je useless ker bo samo tabelo prepisalo znova, ne dela zadnja crka pri sortiranju glede na 1. mesto
+void sort(char tab[A][6]) {
     for (int i = 0; i < A; i++) {
-        for (int j = A - 1; j > i; j--) {
-            if (tab[j - 1][0] > tab[j][0] && tab[j - 1][1] > tab[j][1] && tab[j - 1][2] > tab[j][2] && tab[j - 1][3] > tab[j][3] && tab[j - 1][4] > tab[j][4])
+        for (int j = A - 1; j > i; j--) { // prvi pogoj do || je da lahko dela da zamenja samo 1. crko z '|'
+            if (tab[j - 1][0] == '|' && tab[j][0] != '|' || tab[j - 1][0] > tab[j][0] && tab[j - 1][1] > tab[j][1] && tab[j - 1][2] > tab[j][2] && tab[j - 1][3] > tab[j][3] && tab[j - 1][4] > tab[j][4])
                 switc(tab[j - 1], tab[j]);
         }
     }
 }
 
-void print(char mreza[A][5]) {
+void print(char mreza[A][6]) {
     int z = 0;
     for (int i = 0; i < A && mreza[i][0] != '|'; i++) {
-        for (int j = 0; j < 5; j++)
-            cout << mreza[i][j];
-        cout << endl;
+        cout << mreza[i] << endl;
         z++;
     }
     cout << z << endl;
 }
 
-void vpis(char zel[5], char rum[5]) {
+void vpis(char zel[6], char rum[6], char crn[27]) {
+    for(int i=0;i<6;i++)
+        zel[i] = '\0';
     barva(32);
     cout << "napisi zeleni izraz\n";
-    for (int i = 0; i < 5; i++) {
-        cin >> ws >> noskipws >> zel[i];
-    }
-    barva(96);
-    cout << "napisi rumeni izraz\n";
-    for (int i = 0; i < 5; i++) {
-        cin >> ws >> noskipws >> rum[i];
-    }
     barva(15);
-    cout << endl;
+    gets(zel);
+    for(int i=0;i<6;i++)
+        rum[i] = '\0';
+    barva(96);
+    cout << "napisi rumene crke\n";
+    barva(15);
+    gets(rum);
+    for(int i=0;i<27;i++)
+        crn[i] = '\0';
+    barva(15);
+    cout << "napisi crne crke\n";
+    gets(crn);
+    rum[5] = '\0';
 }
 
 void start() {
     cout << "********************\n";
     cout << "   wordle  solver   \n";
-    cout << "      beta v1.0     \n";
+    cout << "         V1.1       \n";
     cout << "********************\n";
 }
 
-void zl(char mreza[A][5], char zel[5], int in) {
-    for (int i = 0; i < A && mreza[i][0] != '|'; i++) {
-        if (mreza[i][in] != zel[in])
-            for (int j = 0; j < 5; j++)
-                mreza[i][j] = '|';
-    }
-    sort(mreza);
-}
-
-void ru(char mreza[A][5], char rum[5], int in) {
+void ru(char mreza[A][6], char rum[6], int in) {
     int z;
     for (int i = 0; i < A && mreza[i][0] != '|'; i++) {
         z = 0;
         for (int j = 0; j < 5; j++)
             if (mreza[i][j] != rum[in])
                 z++;
-        if (z == 5) {
-            for (int j = 0; j < 5; j++)
-                mreza[i][j] = '|';
+        if (z == 5)
+            mreza[i][0] = '|';
+    }
+    sort(mreza);
+}
+
+void brain(char mreza[A][6], char zel[6],char rum[6], int in) {
+    int z;
+    for (int i = 0; i < A && mreza[i][0] != '|'&&(zel[in]!='\0'||rum[in]!='\0'); i++) {
+        if (mreza[i][in] != zel[in]&&zel[in]!='\0'&&zel[in]>='a'&&zel[in]<='z'){//zelena
+            mreza[i][0] = '|';
+        }
+        z = 0;
+        for (int j = 0; j < 5&&rum[in]!='\0'&&rum[in]>='a'&&rum[in]<='z'; j++)//rumena
+            if (mreza[i][j] != rum[in])
+                z++;
+        if (z == 5){
+            mreza[i][0] = '|';
         }
     }
     sort(mreza);
 }
 
-void decider(char mreza[A][5], char model[5], bool zeru) {
-    int x;
-    if (model[0] >= 'a' && model[0] <= 'z') {
-        if (zeru == true)
-            zl(mreza, model, 0);
-        else
-            ru(mreza, model, 0);
+void cr(char mreza[A][6], char crn[27]) {
+    bool z = 0;
+    for (int i = 0; i < A && mreza[i][0] != '|'&&crn[0]!='\0' ; i++) {
+        z = 0;
+        for (int j = 0; j < 5 && z == 0; j++)
+            for (int k = 0; k < 26 && z == 0&&crn[k]; k++)
+                if (mreza[i][j] == crn[k]) {
+                    mreza[i][0] = '|';
+                    z = 1;
+                }
     }
-    if (model[1] >= 'a' && model[1] <= 'z') {
-        if (zeru == true)
-            zl(mreza, model, 1);
-        else
-            ru(mreza, model, 1);
-    }
-    if (model[2] >= 'a' && model[2] <= 'z') {
-        if (zeru == true)
-            zl(mreza, model, 2);
-        else
-            ru(mreza, model, 2);
-    }
-    if (model[3] >= 'a' && model[3] <= 'z') {
-        if (zeru == true)
-            zl(mreza, model, 3);
-        else
-            ru(mreza, model, 3);
-    }
-    if (model[4] >= 'a' && model[4] <= 'z') {
-        if (zeru == true)
-            zl(mreza, model, 4);
-        else
-            ru(mreza, model, 4);
-    }
+    sort(mreza);
 }
 
-bool check(char mreza[A][5]) {
+
+void deciderv2(char mreza[A][6], char zel[6],char rum[6]) {
+    for(int i=0;i<5;i++)
+        brain(mreza,zel,rum,i);
+}
+
+bool check(char mreza[A][6]) {
     if (mreza[1][0] == '|')
         return 1;
     return 0;
 }
 
-void end(char mreza[A][5]) {
+void end(char mreza[A][6]) {
     cls();
     if (mreza[0][0] != '|') {
         barva(32);
         cout << "********************\n";
         cout << " Bravo tok si neumn \nda ne mors rest sam!\n     hahahahaha     \nbeseda je bila:";
-        for (int i = 0; i < 5; i++)
-            cout << mreza[0][i];
+        cout << mreza[0];
         cout << "\n********************\n";
         barva(15);
     } else {
@@ -179,14 +180,15 @@ void end(char mreza[A][5]) {
 }
 
 int main() {
-    char mreza[A][5];
-    char zel[5], rum[5];
+    char mreza[A][6];
+    char zel[6], rum[6], crn[27]={'\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0','\0',};
+    int mode=0;
     load_file(mreza);
     start();
     while (1) {
-        vpis(zel, rum);
-        decider(mreza, zel, 1);
-        decider(mreza, rum, 0);
+        vpis(zel, rum, crn);
+        cr(mreza,crn);
+        deciderv2(mreza,zel,rum);
         print(mreza);
         if (check(mreza)) {
             end(mreza);
